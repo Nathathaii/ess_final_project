fff9a168b0c203a9a1777308009f7558a120f8fd
Nathathaii <nathathaiaungudomsin@gmail.com>
2023-05-02 22:29:27 +0700

    push frontend

--- front/scriptItem.js
+++ front/scriptItem.js
@@ -1,8 +1,18 @@
-require('dotenv').config();
-const backendIPAddress = process.env.backendIPAddress
+//require("dotenv").config();
+const backendIPAddress = "3.90.97.139:3000";
+// const backendIPAddress = "127.0.0.1:3000";
 
 let itemData;
+var userTasks;
+var userTaskFromDB;
+var userTaskFromCV;
+var foundUserTaskFromDB;
+var id;
 // # 1.GET
+
+const authorizeApplication = () => {
+    window.location.href = `http://${backendIPAddress}/courseville/auth_app`;
+};
 
 //# 1.1 get all data save in a specific student_id
 const getItemsFromDB = async () => {
@@ -10,7 +20,7 @@ const getItemsFromDB = async () => {
         method: "GET",
         credentials: "include",
     };
-    let studentId = '6430204221'; //should change this student_id for different output
+    let studentId = "6430204221"; //should change this student_id for different output
     await fetch(`http://${backendIPAddress}/items/${studentId}`, options)
         .then((response) => response.json())
         .then((data) => {
@@ -21,8 +31,10 @@ const getItemsFromDB = async () => {
 
 //show data from calendar
 const showItemsInTable = async (itemData) => {
-    const table = document.getElementById("") //fill main table body
-    table.innerHTML = "";
+    const table = document.getElementById("myTable"); //fill main table body
+    //table.innerHTML = "";
+    const days = table.getElementsByClassName("day");
+    let sid = "6430204221";
     const tasks = itemData.tasks;
     for (let i = 0; i < tasks.length; i++) {
         const task = tasks[i].task_name;
@@ -32,13 +44,30 @@ const showItemsInTable = async (itemData) => {
         var priority = task.priority;
         var status = task.status;
         var subject_id = task.subject_id;
-        var task_name = task.task_name
+        var task_name = task.task_name;
 
         //เขียนโค้ดฝั่ง front แสดงข้อมูล assignment ลง calendar ได้เลย (อาจใช้แค่ task name กับ duedate)
 
+        for (let j = 0; j < days.length; j++) {
+            //const dayNumber = parseInt(days[j].getElementsByClassName("number")[0].innerHTML);
+            const dayNumber = days[j].getElementsByClassName("number")[0].textContent;
+            if (dayNumber === duedate.slice(-2)) {
+                //??
+                // create a new badge element with the task information
+                const badge = document.createElement("div");
+                badge.classList.add("badge");
+                badge.onclick = function () {
+                    badge_open();
+                };
+                badge.innerHTML = `
+                  <span class="badge-name">${task_name}</span>
+                  <span class="badge-time">${deadline}</span>`;
+                days[j].appendChild(badge);
+                break;
+            }
+        }
     }
-
-}
+};
 
 //#1.2 display only "tasks list" of a specific student
 const getStudentTasks = async () => {
@@ -46,7 +75,7 @@ const getStudentTasks = async () => {
         method: "GET",
         credentials: "include",
     };
-    let studentId = '6430204221'; //should change this student_id for different output
+    let studentId = "6430204221"; //should change this student_id for different output
     await fetch(`http://${backendIPAddress}/items/${studentId}/tasks`, options)
         .then((response) => response.json())
         .then((data) => {
@@ -61,7 +90,7 @@ const getStudentSubjects = async () => {
         method: "GET",
         credentials: "include",
     };
-    let studentId = '6430204221'; //should change this student_id for different output
+    let studentId = "6430204221"; //should change this student_id for different output
     await fetch(`http://${backendIPAddress}/items/${studentId}/subjects`, options)
         .then((response) => response.json())
         .then((data) => {
@@ -77,16 +106,36 @@ const addTask = async () => {
     //const name = document.getElementById("") //event name
     //const date = document.getElementById("")
     //const description = document.getElementById("")
-    const student_id = 'your_student_id';
-    const task_name = 'example task';
-    const description = 'example description';
-    const subject_id = 'example subject id';
-    const priority = 'example type';
-    const duedate = '2023-05-01';
-    const deadline = '2023-05-02';
-    const status = 'example status';
+    const student_id = id;
+    studentId = id;
+    console.log("aaa");
+    // const task_name = document.getElementById("event-name-input").value;
+    // const description = document.getElementsByClassName(
+    //   "event-description-input"
+    // ).value;
+    // const subject_id =
+    //   document.getElementsByClassName("event-course-input").value;
+    // const priority = document.getElementsByClassName("event-type-input").value;
+    // const duedate = [
+    //   document.getElementsByClassName("event-date-date").value,
+    //   document.getElementsByClassName("event-date-month").value,
+    //   document.getElementsByClassName("event-date-year").value,
+    // ];
+    // const deadline = [
+    //   document.getElementsByClassName("event-time-hour").value,
+    //   document.getElementsByClassName("event-time-minute").value,
+    // ];
+    // const status = "todo";
+    const task_name = "name";
+    const description = "descrubtion";
+    const subject_id = "subject";
+    const priority = "priority";
+    const duedate = "date";
+    const deadline = "time";
+    const status = "todo";
 
     const options = {
+        method: "POST",
         headers: {
             "Content-Type": "application/json",
         },
@@ -98,35 +147,39 @@ const addTask = async () => {
             priority: priority,
             duedate: duedate,
             deadline: deadline,
-            status: status
+            status: status,
         }),
         credentials: "include",
     };
-    let studentId = '6430204221';
-    await fetch(`http://${backendIPAddress}/items/${studentId} `, options)
-        .catch((error) => console.error(error))
+    const data = await fetch(
+        `http://${backendIPAddress}/items/${studentId}/tasks`,
+        options
+    ).catch((error) => console.error(error));
+    console.log(data);
 };
 
 // # 2.2 add subjects
 const addSubject = async (studentId, subject) => {
     try {
-        const response = await fetch(`http://${backendIPAddress}/items/${studentId}/subjects`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(subject),
-        });
+        const response = await fetch(
+            `http://${backendIPAddress}/items/${studentId}/subjects`,
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(subject),
+            }
+        );
 
         if (!response.ok) {
-            throw new Error('Error adding subject');
+            throw new Error("Error adding subject");
         }
 
         const data = await response.json();
         console.log(data.message); // Assuming the response contains a 'message' property
 
         // Perform any additional actions upon successful subject addition
-
     } catch (error) {
         console.error(error);
         // Handle error scenario
@@ -143,28 +196,30 @@ const addSubject = async (studentId, subject) => {
 // };
 
 // addSubject(studentId, subject);
-//  
+//
 
 // # 3 PUT update a task's status
 const updateTaskStatus = async (studentId, taskName, newStatus) => {
     try {
-        const response = await fetch(`http://${backendIPAddress}/items/${studentId}/tasks`, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ task_name: taskName, status: newStatus }),
-        });
+        const response = await fetch(
+            `http://${backendIPAddress}/items/${studentId}/tasks`,
+            {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ task_name: taskName, status: newStatus }),
+            }
+        );
 
         if (!response.ok) {
-            throw new Error('Error updating task status');
+            throw new Error("Error updating task status");
         }
 
         const data = await response.json();
         console.log(data.message); // Assuming the response contains a 'message' property
 
         // Perform any additional actions upon successful task status update
-
     } catch (error) {
         console.error(error);
         // Handle error scenario
@@ -179,16 +234,19 @@ const updateTaskStatus = async (studentId, taskName, newStatus) => {
 // updateTaskStatus(studentId, taskName, newStatus);
 
 //-------------------------------------------------------------------
-// # 4. DELETE 
+// # 4. DELETE
 
 const deleteItem = async (item) => {
     const options = {
         method: "DELETE",
         credentials: "include",
     };
-    let student_id = '6430204221';
-    let task_name = 'history_reading';
-    await fetch(`http://${backendIPAddress}/items/${student_id}/tasks/${task_name}`, options)
+    let student_id = "6430204221";
+    let task_name = "history_reading";
+    await fetch(
+        `http://${backendIPAddress}/items/${student_id}/tasks/${task_name}`,
+        options
+    )
         .then((response) => response.json())
         .then((data) => {
             itemData = data;
@@ -196,14 +254,9 @@ const deleteItem = async (item) => {
         .catch((error) => console.error(error));
 };
 
-await getItemsFromDB();
-showItemsInTable(itemData);
-deleteItem();
-
-//------------------------------------- MCV ----------------------------------------------------
-const authorizeApplication = () => {
-    window.location.href = `http://${backendIPAddress}/courseville/auth_app`;
-};
+// await getItemsFromDB();
+// showItemsInTable(itemData);
+// deleteItem();
 
 const getUserProfile = async () => {
     const options = {
@@ -216,21 +269,12 @@ const getUserProfile = async () => {
     )
         .then((response) => response.json())
         .then((data) => {
-            console.log(data.user);
-            var id = data.user.id;
-            var firstname = data.user.firstname_en;
-            var lastname = data.user.lastname_en;
-            return [id, firstname, lastname]; //หรือจะเอา return ออกแล้วแสดง profile ในนี้เลยก้ได้
+            user = data.data.student;
+            console.log(user);
+            id = `${user.id}`;
         })
         .catch((error) => console.error(error));
 };
-
-document.addEventListener("DOMContentLoaded", async function (event) {
-    console.log("Showing items from database.");
-    await getItemsFromDB();
-    showItemsInTable(itemData);
-})
-//---------------------------------------------------------------
 
 const onLoad = async () => {
     await getUserProfile();
@@ -267,3 +311,104 @@ function badge_open() {
 function badge_close() {
     document.getElementById("badge-all").style.display = "none";
 }
+
+const getUserTaskFromCV = async () => {
+    var coursesThisYear = [];
+    const options = {
+        method: "GET",
+        credentials: "include",
+    };
+    await fetch(`http://${backendIPAddress}/courseville/get_courses`, options)
+        .then((response) => response.json())
+        .then((data) => data.data.student)
+        .then((courses) => {
+            console.log(courses);
+            for (var i = 0; i < courses.length; i++) {
+                if (courses[i].year == "2022" && courses[i].semester == 2) {
+                    coursesThisYear.push(courses[i]);
+                }
+            }
+        })
+        .catch((error) => console.error(error));
+    console.log(coursesThisYear);
+    var assignments = [];
+    for (let i = 0; i < coursesThisYear.length; i++) {
+        await fetch(
+            `http://${backendIPAddress}/courseville/get_course_assignments/${coursesThisYear[i].cv_cid}`,
+            options
+        )
+            .then((response) => response.json())
+            .then((data) => data.data)
+            .catch((error) => console.error(error));
+    }
+    userTaskFromCV = assignments;
+};
+
+const getUserTaskFromDB = async () => {
+    const options = {
+        method: "GET",
+        credentials: "include",
+    };
+    await fetch(`http://${backendIPAddress}/items/data`, options)
+        .then((response) => response.json())
+        .then((data) => {
+            for (var i = 0; i < data.length; i++) {
+                if (data[i].id == user.id) {
+                    userTaskFromDB = data[i].task;
+                    foundUserTaskFromDB = true;
+                    break;
+                }
+            }
+        })
+        .catch((error) => console.error(error));
+};
+
+const getUserTask = async () => {
+    foundUserTaskFromDB = false;
+    await getUserTaskFromDB();
+    if (foundUserTaskFromDB) {
+        userTasks = userTaskFromDB;
+        console.log(`${userTasks[i].title}`);
+        await updateUserTaskWithCV();
+    } else {
+        userTasks = [];
+        await updateUserTaskWithCV();
+    }
+};
+
+const updateUserTaskWithCV = async () => {
+    await getUserTaskFromCV();
+    var latestCreateDate = 0;
+    for (var i = 0; i < userTasks.length; i++) {
+        if (userTasks[i].created > latestCreateDate) {
+            latestCreateDate = userTasks[i].created;
+        }
+    }
+
+    for (var i = 0; i < userTaskFromCV.length; i++) {
+        if (userTaskFromCV[i].created > latestCreateDate) {
+            var tmp = userTaskFromCV[i];
+            tmp.importance = 0;
+            userTasks.push(tmp);
+        }
+    }
+};
+
+// const getUserProfile = async () => {
+//   const options = {
+//     method: "GET",
+//     credentials: "include",
+//   };
+//   await fetch(
+//     `http://${backendIPAddress}/courseville/get_profile_info`,
+//     options
+//   )
+//     .then((response) => response.json())
+//     .then((data) => {
+//       user = data.user;
+//       console.log(data.user);
+//     })
+//     .catch((error) => console.error(error));
+// };
+
+// getUserTask();
